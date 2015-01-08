bc7541b32723263fb22b2bc45dee1b68754e5bd0
Joshua Garcia <j.garcia12998@gmail.com>
2015-01-07 17:27:25 -0800

    Lecture 2 Commit

---
+++ Lecture 2.js
@@ -0,0 +1,65 @@
+class Kitten {
+	public $firstName;
+	public $lastName;
+	public $color;
+		function__construct($title, $firstName, $lastName, $color) {
+			$this->firstName = $firstName;
+			$this->firstName = $lastName;
+			$this->color = $color;
+		}
+		function getName() {
+			return "{$this->firstName}";
+			"{$this->lastName}";
+		}
+}
+
+class Friend {
+	public $firstName;
+	public $lastName;
+	public $race;
+		function__construct($title, $firstName, $lastName, $race) {
+			$this->firstName = $firstName;
+			$this->firstName = $lastName;
+			$this->race = $racce;
+		}
+		function getName() {
+			return "{$this->firstName}";
+			"{$this->lastName}";
+		}
+}
+
+class Mom {
+	public $firstName;
+	public $lastName;
+	public $age;
+		function__construct($title, $firstName, $lastName, $age) {
+			$this->firstName = $firstName;
+			$this->firstName = $lastName;
+			$this->age = $age;
+		}
+		function getName() {
+			return "{$this->firstName}";
+			"{$this->lastName}";
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+$kitten1 = new Kitten("Arnold", "Shwarzenegger", "Black");
+print "Kitten 1: {$kitten1->getName()\n}"
+
+$kitten2 = new Kitten("Pink", "Crouch", "Grey");
+print "Kitten2 2: {$kitten2->getName()\n}"
+
+$kitten3 = new Kitten("Robert", "Lewandowski", "White");
+print "Kitten 3: {$kitten3->getName()\n}"
